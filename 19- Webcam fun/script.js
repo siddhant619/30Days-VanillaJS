506d6972beeae62c0a9c1af838c3aea00f464d0e
siddhant619 <siddhantj6.023@gmail.com>
2020-12-31 14:39:25 +0530

    Filter effects added

--- 19- Webcam fun/script.js
+++ 19- Webcam fun/script.js
@@ -3,7 +3,10 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
-
+const redButton=document.getElementById('red');
+const grayButton=document.getElementById('gray');
+const noneButton=document.getElementById('none');
+let effect='';//set this to the function name for the particular effect on btn click
 function getVideo(){
     navigator.mediaDevices.getUserMedia({video:true,audio:false})
         .then( (localMediaStream)=>{
@@ -24,7 +27,38 @@ function showVideoOnCanvas(){
     canvas.height=height;
     setInterval( function(){
         ctx.drawImage(video,0,0,width,height);
-    },16 )
+        let pixels=ctx.getImageData(0,0,width,height);
+        window[effect](pixels);//GFG- https://www.geeksforgeeks.org/how-to-call-function-from-it-name-stored-in-a-string-using-javascript/
+        /* if(effect=='red')
+            pixels=redEffect(pixels);
+        else if(effect=='gray')
+            pixels=grayEffect(pixels); */
+         
+        ctx.putImageData(pixels,0,0);
+        
+    },160 )
+}
+
+function redEffect(pixels){
+    //pixels is not a normal array- so cant apply map method
+    for(let i=0; i<pixels.data.length;i+=4){
+        pixels.data[i+0]+=200;//R
+        pixels.data[i+1]-=50;//G
+        pixels.data[i+0]*=0.5;//B
+    }
+    return pixels;
+}
+function grayEffect(pixels){
+    let pix=pixels.data;
+    for(let i=0; i<pixels.data.length;i+=4){
+        //This formula calculates the luminance of a color as it’s perceived by the human eye, so the green channel has more importance that the red and the blue
+        //https://spyrestudios.com/html5-canvas-image-effects-black-white/
+        var grayscale = pix[i ] * .3 + pix[i+1] * .59 + pix[i+2] * .11;
+        pix[i ] = grayscale; // red
+        pix[i+1] = grayscale; // green
+        pix[i+2] = grayscale; // blue
+    }
+    return pixels;
 }
 
 function takeSnap(){
@@ -48,4 +82,14 @@ document.getElementById('takesnap').addEventListener('click',function(){
     takeSnap();
 })
 
+redButton.addEventListener('click', ()=>{
+    effect='redEffect';
+})
+
+grayButton.addEventListener('click', ()=>{
+    effect='grayEffect';
+})
+noneButton.addEventListener('click', ()=>{
+    effect='';
+})
 
